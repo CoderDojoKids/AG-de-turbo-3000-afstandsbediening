e0dc77e2e27dc962a2d3d263dd01fab94f2c8811
CoderDojoKids <128733393+CoderDojoKids@users.noreply.github.com>
2023-06-24 11:39:16 +0200

    betere besturing

--- main.ts
+++ main.ts
@@ -1,16 +1,31 @@
 input.onGesture(Gesture.LogoDown, function () {
     radio.sendNumber(9)
 })
-input.onButtonPressed(Button.A, function () {
-    radio.sendNumber(8)
+input.onGesture(Gesture.Shake, function () {
+    radio.sendNumber(5)
 })
-input.onButtonPressed(Button.AB, function () {
-    radio.sendNumber(7)
-})
-input.onButtonPressed(Button.B, function () {
+input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(6)
 })
+input.onGesture(Gesture.LogoUp, function () {
+    radio.sendNumber(7)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    radio.sendNumber(8)
+})
+for (let index = 0; index < 3; index++) {
+    basic.showIcon(IconNames.Yes)
+    basic.showIcon(IconNames.Tortoise)
+    basic.showIcon(IconNames.Sword)
+    basic.clearScreen()
+}
 radio.setGroup(7)
 basic.forever(function () {
-	
+    basic.showLeds(`
+        # . # . #
+        # # # # #
+        . # # # .
+        . # # # .
+        # # # # #
+        `)
 })
